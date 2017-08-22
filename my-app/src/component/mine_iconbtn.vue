8db84f5c4f7e9ec2d5f5db5585a45bf2fe68a188
tl <541972091@qq.com>
2017-08-22 14:12:25 +0800

    tll

---
+++ my-app/src/component/mine_iconbtn.vue
@@ -0,0 +1,20 @@
+<template>
+    <f7-col :width="width">
+        <f7-link :href="link">
+            <div class="iconbtn">
+                <img :src="href" width="25" height="25">
+                <div>{{title}}</div>
+            </div>
+        </f7-link>
+    </f7-col>
+</template>
+<script>
+    export default{
+        props:['title','img','href','width']
+    }
+</script>
+<style>
+    .iconbtn{
+        text-align:center;
+    }
+</style>
--- my-app/src/pages/mine.vue
+++ my-app/src/pages/mine.vue
@@ -1,16 +1,67 @@
 <template>
-<div>
-    <div><button> 登录</button></div>
-    <div><button> 登录</button></div>
-    <div><button> 登录</button></div>
-    <div><button> 登录</button></div>
-    <div><button> 登录</button></div>
-    <div><button> 登录</button></div>
-    <div><button> 登录</button></div>
-    <div><button> 登录</button></div>
-    <div><button> 登录</button></div>
-    <div><button> 登录</button></div>
-    <div><button> 登录</button></div>
-    <div><button> 登录</button></div>
-</div>
+    <div class="">
+        <f7-navbar  title="我的" sliding>   
+        </f7-navbar>
+        <f7-block>
+            <f7-link  @click=" openPopup">
+                <div class="content-block-title"><img src="">登录/注册</div>
+            </f7-link>
+            <f7-grid > 
+                <mine-iconbtn v-for="mine in mine" :title="mine.title"  :href="mine.href" width="20"></mine-iconbtn>                 
+            </f7-grid>
+            <f7-grid > 
+                <mine-iconbtn v-for="mine in mine" :title="mine.title"  :href="mine.href" width="20"></mine-iconbtn>                 
+            </f7-grid>
+            <f7-link href="/about/">
+                <div class="content-block-title">败家姐<i class="iconfont icon-xiangyou"></i></div>
+            </f7-link>
+        </f7-block>
+    </div>
 </template>
+<script>
+    import MineIconbtn from '../component/mine_iconbtn.vue'
+    import mine from '../json/mine.json'
+    export default{
+        components:{
+            'mine-iconbtn': MineIconbtn
+        },
+        data(){
+            return{
+                mine: []
+            }
+        },
+        methods:{
+            getData(){
+                this.mine = mine.data
+            },
+            openPopup() {
+                this.$f7.popup('#popup')
+            },
+        },
+        mounted (){
+            this.getData()
+        }
+    }
+</script>
+<style scoped>
+    .content-block{
+        padding:0px;
+        margin:0px;
+    }
+    .content-block-title{
+        background:#fff;
+        margin:10px 0;
+        padding:10px;
+    }
+    .row{
+        background:#fff;
+        padding:10px;
+    }
+    .icon-xiangyou{
+        float:right;
+    }
+    img{
+        width:20px;
+        height:20px;
+    }
+</style>
